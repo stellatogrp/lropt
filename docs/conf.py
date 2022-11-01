2d33b8c464750e86d29883c0b03c153fc22a37d7
colbeck <cb35@princeton.edu>
2022-10-31 21:44:31 -0400

    docs

---
+++ docs/conf.py
@@ -0,0 +1,26 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'lro docs'
+copyright = '2022, Irina Wang, Cole Becker, Bart Van Parys, Bartolomeo Stellato'
+author = 'Irina Wang, Cole Becker, Bart Van Parys, Bartolomeo Stellato'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. lro documentation master file, created by
+   sphinx-quickstart on Mon Oct 31 18:19:48 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to lro's documentation!
+===============================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
