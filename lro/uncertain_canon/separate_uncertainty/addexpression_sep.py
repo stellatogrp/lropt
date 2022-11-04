e7b1575ea919281e3fa0d7dd334b0e03d350c200
Irina Wang <iywang@princeton.edu>
2022-11-04 11:02:24 -0400

    fix multiple args for add

--- lro/uncertain_canon/separate_uncertainty/addexpression_sep.py
+++ lro/uncertain_canon/separate_uncertainty/addexpression_sep.py
@@ -1,14 +1,12 @@
+
 def addexpression_sep(unc_canon, expr):
-    if unc_canon.has_unc_param(expr.args[0]) and \
-            unc_canon.has_unc_param(expr.args[1]):
-        unc_lst_0, std_lst_0 = unc_canon.separate_uncertainty(expr.args[0])
-        unc_lst_1, std_lst_1 = unc_canon.separate_uncertainty(expr.args[1])
-        return (unc_lst_0 + unc_lst_1, std_lst_0 + std_lst_1)
-
-    unc_param, non_unc_param = expr.args
-    if unc_canon.has_unc_param(non_unc_param):
-        non_unc_param, unc_param = expr.args
-
-    unc_lst, std_lst = unc_canon.separate_uncertainty(unc_param)
-    std_lst.append(non_unc_param)
+    unc_lst = []
+    std_lst = []
+    for arg in expr.args:
+        if unc_canon.has_unc_param(arg):
+            unc_lst_0, std_lst_0 = unc_canon.separate_uncertainty(arg)
+            unc_lst = unc_lst + unc_lst_0
+            std_lst = std_lst + std_lst_0
+        else:
+            std_lst.append(arg)
     return (unc_lst, std_lst)
