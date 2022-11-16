d866e2c723d978ea441454f16bc073f425523687
Irina Wang <iywang@princeton.edu>
2022-11-16 16:33:22 -0500

    add test notebooks

--- lro/__init__.py
+++ lro/__init__.py
@@ -1,6 +1,7 @@
 from lro.robust_problem import RobustProblem
 from lro.settings import OPTIMIZERS
 from lro.uncertain import UncertainParameter
+from lro.uncertain_atoms.quad_form import quad_form
 from lro.uncertainty_sets.box import Box
 from lro.uncertainty_sets.budget import Budget
 from lro.uncertainty_sets.ellipsoidal import Ellipsoidal
