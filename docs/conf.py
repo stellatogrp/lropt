b03c6cec5d2777913239c51e2cbfa418c06d388e
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2023-02-20 17:10:21 -0500

    prepare for 0.0.1 release

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'LRO documentation'
-copyright = '2022, Irina Wang, Cole Becker, Bart Van Parys, Bartolomeo Stellato'
+copyright = '2023, Irina Wang, Cole Becker, Bart Van Parys, Bartolomeo Stellato'
 author = 'Irina Wang, Cole Becker, Bart Van Parys, Bartolomeo Stellato'
 
 # -- General configuration ---------------------------------------------------
