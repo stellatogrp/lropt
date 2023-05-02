5720e6dfc11e3038e05a37829411f8ac13d79e37
colbeck <cb35@princeton.edu>
2023-05-02 14:25:22 -0400

    Family Parameter -> Parameter

--- lropt/parameter.py
+++ lropt/parameter.py
@@ -2,7 +2,7 @@ import cvxpy as cp
 import numpy as np
 
 
-class FamilyParameter(cp.Parameter):
+class Parameter(cp.Parameter):
     def __init__(self, *args, **kwargs):
         instances = kwargs.pop('instances', None)
 
@@ -10,7 +10,7 @@ class FamilyParameter(cp.Parameter):
             raise ValueError("You must provide instances")
         assert isinstance(instances, np.ndarray), "not a numpy array"
 
-        super(FamilyParameter, self).__init__(*args, **kwargs)
+        super(Parameter, self).__init__(*args, **kwargs)
 
         assert (instances.shape[1:] == self.shape)
 
