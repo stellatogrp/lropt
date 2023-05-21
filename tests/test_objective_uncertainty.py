af211f027abbc393299a2c19875e1691d0c70516
colbeck <cb35@princeton.edu>
2023-05-21 14:51:10 -0400

    objective uncertainty #4

--- tests/test_objective_uncertainty.py
+++ tests/test_objective_uncertainty.py
@@ -5,17 +5,15 @@ import cvxpy as cp
 # import matplotlib.pyplot as plt
 import numpy as np
 import numpy.random as npr
-import numpy.testing as npt
 
+# import numpy.random as npr
 from lropt.robust_problem import RobustProblem
 from lropt.uncertain import UncertainParameter
 from lropt.uncertainty_sets.box import Box
 from lropt.uncertainty_sets.ellipsoidal import Ellipsoidal
-from tests.settings import TESTS_ATOL as ATOL
-from tests.settings import TESTS_RTOL as RTOL
 
 
-class TestMultipleUncertainty(unittest.TestCase):
+class TestObjectiveUncertainty(unittest.TestCase):
 
     def setUp(self):
         """Setup basic problem"""
@@ -29,9 +27,12 @@ class TestMultipleUncertainty(unittest.TestCase):
         self.rho = 0.2
         self.p = 2
 
-    def test_simple_ellipsoidal_2u(self):
 
-        # SETUP
+    def test_objective_uncertainty(self):
+
+        # import ipdb
+        # ipdb.set_trace()
+
         n = 5
         x_lropt = cp.Variable(n)
         c = npr.rand(n)
@@ -41,13 +42,14 @@ class TestMultipleUncertainty(unittest.TestCase):
         rho_1 = 0.2
         rho_2 = 0.5
 
-        objective_1 = cp.Minimize(c @ x_lropt)
+        # objective_1 = cp.Minimize(c @ x_lropt)
         # Formulate robust constraints with lropt
         unc_set_1 = Ellipsoidal(rho=rho_1)
         unc_set_2 = Box(rho=rho_2)
         u_1 = UncertainParameter(n,
                                  uncertainty_set=unc_set_1)
 
+        objective_1 = cp.Minimize(u_1 @ x_lropt + x_lropt @ c)
         u_2 = UncertainParameter(n,
                                  uncertainty_set=unc_set_2)
         constraints_1 = [cp.maximum(u_1 @ P @ x_lropt, a @ x_lropt) + u_2 @ x_lropt <= b]
@@ -58,10 +60,22 @@ class TestMultipleUncertainty(unittest.TestCase):
         # CVXPY problem
 
         x_cvx = cp.Variable(n)
-        objective_2 = cp.Minimize(x_cvx @ c)
+
         tau_1 = cp.Variable()
         tau_2 = cp.Variable()
+        # tau_3 = cp.Variable()
 
+
+        u_1 = UncertainParameter(n,
+                                 uncertainty_set=unc_set_1)
+        u_2 = UncertainParameter(n,
+                                 uncertainty_set=unc_set_2)
+
+        objective_2 = cp.Minimize(u_1 @ x_cvx + x_cvx @ c)
+
+
+
+        # constraints_2 = [x_cvx @ u_1 <= tau_3]
         constraints_2 = [tau_1 + tau_2 - b <= 0]
         constraints_2 += [cp.maximum(u_1 @ P @ x_cvx, a @ x_cvx) <= tau_1]
         constraints_2 += [u_2 @ x_cvx <= tau_2]
@@ -72,21 +86,4 @@ class TestMultipleUncertainty(unittest.TestCase):
         import ipdb
         ipdb.set_trace()
 
-        npt.assert_allclose(x_lropt.value, x_cvx.value, rtol=RTOL, atol=ATOL)
-
-    def test_matrix(self):
-        n = 5
-        # X = cp.Variable((n, n))
-        x = cp.Variable(n)
-        A = npr.randint(-5, 5, size=(n, n))
-        b = 5
-
-        c = npr.randint(-5, 5, size=n)
-
-        obj = cp.Minimize(c @ x)
-
-        constr = [cp.norm(A @ x)**2 <= b]
-
-        prob = cp.Problem(obj, constr)
-
-        prob.solve()
+        # npt.assert_allclose(x_cvxpy, x_robust, rtol=RTOL, atol=ATOL)
