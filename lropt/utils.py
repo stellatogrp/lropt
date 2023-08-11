9d73262cecb945417dbb2723baa0aef31a1fe9c0
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2023-08-11 17:25:17 -0400

    added black formatter settings to match ruff

--- lropt/utils.py
+++ lropt/utils.py
@@ -1,13 +1,12 @@
-
 class UncertaintyError(Exception):
-    """Error thrown if the uncertain problem has not been formulated correctly.
-    """
+    """Error thrown if the uncertain problem has not been formulated correctly."""
+
     pass
 
 
 def check_affine_transform(affine_transform):
-    assert 'b' in affine_transform
-    assert 'A' in affine_transform
+    assert "b" in affine_transform
+    assert "A" in affine_transform
 
 
 def unique_list(duplicates_list):
@@ -17,9 +16,9 @@ def unique_list(duplicates_list):
     """
     used = set()
     unique = [x for x in duplicates_list if not (x in used or used.add(x))]
+
     return unique
 
 
 def cvxpy_to_torch(cvxpy_expr, dec_var_dict, unc_param_dict, fam_param_dict):
-
     return None
