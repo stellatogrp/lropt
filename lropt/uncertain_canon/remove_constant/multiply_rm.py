ffbd4063fb1f39a33b4e68991b10d338cd0d4564
Irina Wang <iywang@princeton.edu>
2023-12-14 15:19:42 -0500

    support elementwise multiply

--- lropt/uncertain_canon/remove_constant/multiply_rm.py
+++ lropt/uncertain_canon/remove_constant/multiply_rm.py
@@ -1,3 +1,4 @@
+from cvxpy.atoms.affine.diag import diag
 from cvxpy.atoms.affine.promote import Promote
 from cvxpy.atoms.affine.unary_operators import NegExpression
 
@@ -8,12 +9,16 @@ def multiply_rm(unc_canon, expr, constant):
             constant = (constant*expr.args[0]).value
         elif isinstance(expr.args[0], Promote):
             constant = (constant*expr.args[0].args[0]).value
+        else:
+            return diag(expr.args[0])@expr.args[1], constant
         return unc_canon.remove_constant(expr.args[1], constant)
     if expr.args[1].is_constant() and not unc_canon.has_unc_param(expr.args[1]):
         if expr.args[1].is_scalar():
             constant = (constant*expr.args[1]).value
         elif isinstance(expr.args[1], Promote):
             constant = (constant*expr.args[1].args[0]).value
+        else:
+            return diag(expr.args[1])@expr.args[0], constant
         return unc_canon.remove_constant(expr.args[0], constant)
     if isinstance(expr.args[0], NegExpression) and isinstance(expr.args[1], NegExpression):
         return unc_canon.remove_constant(expr.args[0].args[0]*expr.args[1].args[0], constant)
