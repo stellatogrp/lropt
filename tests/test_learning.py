d357b02b4d5fe368c67ebe5ccab14fe16a43d317
Irina Wang <iywang@princeton.edu>
2024-12-04 16:12:52 -0500

    add test - fails

--- tests/test_learning.py
+++ tests/test_learning.py
@@ -175,10 +175,10 @@ class TestEllipsoidalUncertainty(unittest.TestCase):
         # Generate data
         data = gen_demand_intro(N, seed=5)
 
-        num_context = 10
+        num_scenarios = 10
         num_reps = int(N/10)
-        p_data = p + np.random.normal(0,1,(num_context,n))
-        k_data = k + np.random.normal(0,1,(num_context,n))
+        p_data = p + np.random.normal(0,1,(num_scenarios,n))
+        k_data = k + np.random.normal(0,1,(num_scenarios,n))
         p_data = np.vstack([p_data]*num_reps)
         k_data = np.vstack([k_data]*num_reps)
 
@@ -271,3 +271,81 @@ class TestEllipsoidalUncertainty(unittest.TestCase):
         assert f_tch(x_test) == prob.problem_canon.f(*vars_test)
         assert g1_tch(x_test, u_test, y_test) == prob.problem_canon.g[0](*vars_test)
         assert len(prob.g)==1 #The second constraint is not saved to g since it has no uncertainty
+
+    def test_news_learning(self):
+        n = 2
+        N = 500
+        k = np.array([4.,5.])
+        p = np.array([5,6.5])
+
+        def gen_demand_intro(N, seed):
+            np.random.seed(seed)
+            sig = np.array([[0.6,-0.3],[-0.3,0.1]])
+            mu = np.array((1.1,1.7))
+            norms = np.random.multivariate_normal(mu,sig, N)
+            d_train = np.exp(norms)
+            return d_train
+
+        # Generate data
+        data = gen_demand_intro(N, seed=18)
+
+        num_scenarios = N
+        num_reps = int(N/num_scenarios)
+        k_data = k + np.random.normal(0,0.5,(num_scenarios,n))
+        p_data = k_data + np.maximum(0,np.random.normal(0,0.5,(num_scenarios,n)))
+        p_data = np.vstack([p_data]*num_reps)
+        k_data = np.vstack([k_data]*num_reps)
+
+        # Formulate uncertainty set
+        u = UncertainParameter(n,
+                                uncertainty_set=Ellipsoidal(
+                                                            data=data))
+        # Formulate the Robust Problem
+        x_r = cp.Variable(n)
+        t = cp.Variable()
+        k = ContextParameter(2, data=k_data)
+        p = ContextParameter(2, data=p_data)
+        p_x = cp.Variable(n)
+        objective = cp.Minimize(t)
+        constraints = [max_of_uncertain([-p[0]*x_r[0] - p[1]*x_r[1],
+                                         -p[0]*x_r[0] - p_x[1]*u[1],
+                                         -p_x[0]*u[0] - p[1]*x_r[1],
+                                           -p_x[0]*u[0]- p_x[1]*u[1]]) +
+                                           k@x_r <= t]
+        constraints += [p_x == p]
+        constraints += [x_r >= 0]
+
+        eval_exp = k@x_r + cp.maximum(-p[0]*x_r[0] - p[1]*x_r[1],
+                                      -p[0]*x_r[0] - p[1]*u[1], -p[0]*u[0] -
+                                      p[1]*x_r[1], -p[0]*u[0]- p[1]*u[1])
+
+        prob = RobustProblem(objective, constraints,eval_exp = eval_exp)
+        test_p = 0.9
+        s = 8
+
+        # setup intial A, b
+        train, test = train_test_split(data,
+                                       test_size=int(data.shape[0]*test_p),
+                                       random_state=s)
+
+        np.random.seed(15)
+        initn = sc.linalg.sqrtm(np.cov(train.T))
+        init_bvaln = np.mean(train, axis=0)
+        # Train A and b
+        from lropt import Trainer
+        trainer = Trainer(prob)
+        result = trainer.train(lr=0.001, train_size = False,
+                               num_iter=50, optimizer="SGD",seed=5,
+                               init_A=initn, init_b=init_bvaln,
+                               init_lam=0.1, init_mu=0.1,
+                            mu_multiplier=1.001, kappa=0.,
+                            init_alpha=0., test_percentage = test_p,
+                            save_history = True,
+                            quantiles = (0.4,0.6),
+                            lr_step_size = 50, lr_gamma = 0.5,
+                            random_init = True, num_random_init = 5,
+                            parallel = False, position = False,
+                            eta=0.3, contextual = True)
+        result.df
+        # A_fin = result.A
+        # b_fin = result.b
